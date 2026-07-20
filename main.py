3064ec1a23d806525842e438fffc34cbd04d752b
sparkkdusk <moniraamim18@gmail.com>
2026-07-20 13:22:25 +0600

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -80,3 +80,37 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+
+
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskCreate):
+
+    for task in tasks:
+        if task["id"] == task_id:
+
+            task["title"] = updated_task.title
+
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+
+    for task in tasks:
+
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
